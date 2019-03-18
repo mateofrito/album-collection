a4f38f356c743a3c59e3cabfca7154b6b44faabe
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-03-18 14:42:35 -0400

    fun times

--- src/main/java/org/wecancodeit/albumapp/controllers/HomeController.java
+++ src/main/java/org/wecancodeit/albumapp/controllers/HomeController.java
@@ -1,5 +1,15 @@
 package org.wecancodeit.albumapp.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
 public class HomeController {
+	
+	//@GetMapping("/")
+	public String home() {
+		return "index";
+	}
 
 }
