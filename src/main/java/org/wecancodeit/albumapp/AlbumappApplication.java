f863a3e8833852255c852f20631fc8de260cd7ce
mateofrito <46681151+mateofrito@users.noreply.github.com>
2019-03-18 13:24:53 -0400

    spring intialized

---
+++ src/main/java/org/wecancodeit/albumapp/AlbumappApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.albumapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumappApplication.class, args);
+	}
+
+}
